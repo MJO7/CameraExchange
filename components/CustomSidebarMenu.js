40d0ff27f4315fb46631f7399f1b65fed2b1026d
MJO7 <megh07joshi@gmail.com>
2021-05-16 16:25:55 +0530

    fifth commit

--- components/CustomSidebarMenu.js
+++ components/CustomSidebarMenu.js
@@ -11,14 +11,40 @@ export default class CustomSidebarMenu extends React.Component {
           <DrawerItems {...this.props} />
         </View>
         <TouchableOpacity
+          style={[styles.inputBox2, { width: 260 }]}
           onPress={() => {
             this.props.navigation.navigate("WelcomeScreen");
             firebase.auth().signOut();
           }}
         >
-          <Text>LOGOUT</Text>
+          <Text
+            style={{
+              fontSize: 23,
+              fontWeight: "bold",
+              color: "#61C494",
+            }}
+          >
+            LOGOUT
+          </Text>
         </TouchableOpacity>
       </View>
     );
   }
 }
+const styles = StyleSheet.create({
+  inputBox2: {
+    borderWidth: 1,
+    alignSelf: "center",
+    borderColor: "white",
+    height: 60,
+    backgroundColor: "#1B5788",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 4,
+    width: 320,
+    borderRadius: 15,
+    marginTop: 20,
+    fontSize: 22,
+    color: "white",
+  },
+});
--- screens/MyAccount.js
+++ screens/MyAccount.js
@@ -13,6 +13,8 @@ import {
   ImageBackground,
   TextInput,
 } from "react-native";
+import db from "../config";
+import firebase from "firebase";
 // import ImagePicker from "react-native-image-picker";
 // import { showImagePicker } from "react-native-image-picker";
 // const options = {
@@ -37,6 +39,48 @@ import {
 //   }
 // });
 export default class MyAccount extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      emailId: "",
+      firstName: "",
+      lastName: "",
+      Address: "",
+      Contact: "",
+      docId: "",
+    };
+  }
+  updateDetals = () => {
+    db.collection("users").doc(this.state.docId).update({
+      first_name: this.state.firstName,
+      last_name: this.state.lastName,
+      contact: this.state.Contact,
+      address: this.state.Address,
+    });
+    Alert.alert("Details Udpated Successfully");
+  };
+  getUserDetails = () => {
+    var emailId = firebase.auth().currentUser.email;
+    db.collection("users")
+      .where("emailId", "==", emailId)
+      .get()
+      .then((snapshot) => {
+        snapshot.forEach((doc) => {
+          var data = doc.data();
+          this.setState({
+            emailId: data.email_id,
+            firstName: data.first_name,
+            lastName: data.last_name,
+            Address: data.address,
+            Contact: data.contact,
+            docId: doc.id,
+          });
+        });
+      });
+  };
+  componentDidMount() {
+    this.getUserDetails();
+  }
   render() {
     return (
       <ImageBackground style={styles.bg} source={require("../assets/bg3.jpg")}>
@@ -46,6 +90,55 @@ export default class MyAccount extends React.Component {
             source={require("../assets/camera.png")}
             style={styles.imageStyle}
           />
+          <TextInput
+            style={styles.inputBox2}
+            placeholder="First Name"
+            placeholderTextColor="#61C494"
+            onChangeText={(text) => {
+              this.setState({ firstName: text });
+            }}
+            value={this.state.firstName}
+          />
+          <TextInput
+            style={styles.inputBox2}
+            placeholder="Last Name"
+            placeholderTextColor="#61C494"
+            onChangeText={(text) => {
+              this.setState({ lastName: text });
+            }}
+            value={this.state.lastName}
+          />
+          <TextInput
+            style={styles.inputBox2}
+            placeholder="Contact"
+            placeholderTextColor="#61C494"
+            onChangeText={(text) => {
+              this.setState({ Contact: text });
+            }}
+            value={this.state.Contact}
+          />
+          <TextInput
+            style={styles.inputBox2}
+            placeholder="Address"
+            placeholderTextColor="#61C494"
+            onChangeText={(text) => {
+              this.setState({ Address: text });
+            }}
+            value={this.state.Address}
+          />
+          <TouchableOpacity
+            style={styles.updateButton}
+            onPress={() => {
+              this.updateDetals();
+            }}
+          >
+            <Text
+              style={{ fontSize: 40, fontWeight: "bold", color: "#61C494" }}
+              i
+            >
+              Update
+            </Text>
+          </TouchableOpacity>
         </View>
       </ImageBackground>
     );
@@ -72,6 +165,21 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  inputBox2: {
+    borderWidth: 1,
+    alignSelf: "center",
+    borderColor: "white",
+    height: 60,
+    backgroundColor: "#1B5788",
+    alignItems: "center",
+    justifyContent: "center",
+    borderWidth: 4,
+    width: 320,
+    borderRadius: 15,
+    marginTop: 20,
+    fontSize: 22,
+    color: "white",
+  },
   imageStyle: {
     marginLeft: 2,
     marginTop: -70,
@@ -85,5 +193,21 @@ const styles = StyleSheet.create({
     backgroundColor: "#61C494",
     width: 280,
     borderColor: "#1B5788",
+  },
+  updateButton: {
+    fontSize: 40,
+    fontWeight: "bold",
+    marginTop: 50,
+    marginLeft: 30,
+    width: 360,
+    color: "#61C494",
+    borderColor: "white",
+    backgroundColor: "#1B5788",
+    justifyContent: "center",
+    borderWidth: 4,
+    borderRadius: 17,
+    textAlign: "center",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
